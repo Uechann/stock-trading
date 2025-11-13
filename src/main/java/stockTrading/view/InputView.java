f9e1654756e8e3912c95906e91cba2e3fdc7f1d0
나의찬 <cab07036@naver.com>
2025-11-14 01:52:39 +0900

    feat(view): InputView 클래스 추가
    InputView를 통해 입력 받는 기능을 구현하였습니다.

---
+++ src/main/java/stockTrading/view/InputView.java
@@ -0,0 +1,28 @@
+package stockTrading.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public InputView() {}
+
+    public String inputSymbols() {
+        System.out.println("거래할 주식 종목을 입력하세요. (예: APPL,GOOG,TSLA)");
+        return Console.readLine();
+    }
+
+    public String inputAccounts() {
+        System.out.println("계좌 번호를 입력하세요. (예: 3333-12-1234567)");
+        return Console.readLine();
+    }
+
+    public String inputAccountFunds() {
+        System.out.println("계좌 초기 자금을 입력하세요. (예: 1000000)");
+        return Console.readLine();
+    }
+
+    public String inputAccountSymbolQuantity() {
+        System.out.println("계좌 초기 종목 보유량을 입력하세요");
+        return Console.readLine();
+    }
+}
