9675a1e09519f472b98cb3bdcf146219872d35ea
나의찬 <cab07036@naver.com>
2025-11-14 21:30:37 +0900

    refactor(Symbol): 종목 record 자료형으로 수정
    기존에 종목은 class로 관리하였지만 불면의 VO 값 객체라는 것을 깨닫고 record값으로 수정하였습니다.

--- src/main/java/stockTrading/domain/model/Symbol.java
+++ src/main/java/stockTrading/domain/model/Symbol.java
@@ -3,16 +3,14 @@ package stockTrading.domain.model;
 import static stockTrading.global.Exception.ErrorMessage.SYMBOL_NAME_PATTERN_NOT_MATCH;
 import static stockTrading.global.constant.Pattern.SYMBOL_NAME_REGEX;
 
-public class Symbol {
-    private final String name;
+/**
+ * 값 객체 VO는 ID가 필요 없고 Repository에서 관리하고
+ * 값 객체에 대한 유효성 검사 추가 등은 Registry에서 관리한다.
+ */
+public record Symbol(String name) {
 
-    public Symbol(String name) {
+    public Symbol {
         validate(name);
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 
     // ============== private method ======================
