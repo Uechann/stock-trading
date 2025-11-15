831fd7088da9c0aafdb0b89dbdb606e0611beb3a
나의찬 <cab07036@naver.com>
2025-11-16 03:59:57 +0900

    refactor(OrderTest): 불필요한 import, setUp 제거

--- src/test/java/stockTrading/domain/model/OrderTest.java
+++ src/test/java/stockTrading/domain/model/OrderTest.java
@@ -1,52 +1,12 @@
 package stockTrading.domain.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import stockTrading.domain.repository.AccountRepository;
-import stockTrading.domain.repository.SymbolRegistry;
-import stockTrading.domain.service.OrderValidator;
 import stockTrading.dto.OrderRequest;
-import stockTrading.infra.InMemoryAccountRepository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class OrderTest {
-
-    private AccountRepository accountRepository;
-    private SymbolRegistry symbolRegistry;
-    private OrderValidator orderValidator;
-
-    @BeforeEach
-    void setUp() {
-        // given
-        accountRepository = new InMemoryAccountRepository();
-        Account accountA = new Account("3333-11-1234567", 4_000_000);
-        Account accountB = new Account("3333-22-1234567", 4_000_000);
-        accountRepository.add(accountA);
-        accountRepository.add(accountB);
-
-        Symbols symbols = new Symbols();
-        symbols.add(new Symbol("AAPL"));
-        symbols.add(new Symbol("GOOG"));
-        symbolRegistry = new SymbolRegistry(symbols);
-
-        AccountSymbols accountSymbolsA = new AccountSymbols();
-        AccountSymbol accountSymbolA1 = AccountSymbol.of(new Symbol("APPL"), 100);
-        AccountSymbol accountSymbolA2 = AccountSymbol.of(new Symbol("GOOG"), 100);
-        accountSymbolsA.add(accountSymbolA1);
-        accountSymbolsA.add(accountSymbolA2);
-        accountA.initializeSymbolQuantities(accountSymbolsA);
-
-        AccountSymbols accountSymbolsB = new AccountSymbols();
-        AccountSymbol accountSymbol1B1 = AccountSymbol.of(new Symbol("APPL"), 100);
-        AccountSymbol accountSymbol1B2 = AccountSymbol.of(new Symbol("GOOG"), 100);
-        accountSymbolsB.add(accountSymbol1B1);
-        accountSymbolsB.add(accountSymbol1B2);
-        accountB.initializeSymbolQuantities(accountSymbolsB);
-
-        orderValidator = new OrderValidator();
-    }
 
     @Test
     @DisplayName("주문이 해당 입력 대로 생성되는지 검증하는 성공 테스트")
